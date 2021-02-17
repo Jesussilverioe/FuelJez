ffa0207374b5332563e769c66a9087a4bae6f597
Aryanp10 <aaryanparajulee@gmail.com>
2021-02-16 18:30:05 -0600

    faq and checkout page

---
+++ main.py
@@ -0,0 +1,63 @@
+# from flask import Flask
+# from flask import render_template, request, url_for, redirect, g, session, flash
+# import sqlite3
+# import random, string
+# from decimal import Decimal
+
+# # def fileReader():
+# #     pdfName = "old/file.pdf"
+
+# #     pdfRead = PyPDF2.PdfFileReader(pdfName)
+
+# #     for i in range(pdfRead.getNumPages()):
+# #         page = pdfRead.getPage(i)
+# #         print("Page No: " + str(1 + pdfRead.getPageNumber(page)))
+# #         pageContent = page.extractText()
+# #         print(pageContent)
+
+
+# app = Flask(__name__)
+# app.secret_key = "123"
+
+
+# def genID(length):
+#     id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(length)])
+#     return id
+
+
+
+# @app.route("/", methods=["POST", "GET"])
+# def index():
+#     if request.method == "POST":
+#         session['gallons_requested'] = request.form['gallons_requested']
+#         session['delivery_address'] = request.form['delivery_address']
+#         session['delivery_date'] = request.form['delivery_date']  
+
+#         print(session)
+
+#         return render_template("checkout.html", gallons_requested = session['gallons_requested'])
+
+
+#     else:
+#         return render_template("index.html")
+
+
+# @app.route("/checkout", methods=["POST", "GET"])
+# def checkout():
+#     return render_template("checkout.html")
+#     # if request.method == "POST":
+#     #     session['gallons_requested'] = request.form['gallons_requested']
+#     #     session['delivery_address'] = request.form['delivery_address']
+#     #     session['delivery_date'] = request.form['delivery_date']  
+
+#     #     print(session)
+#     #     return redirect(url_for("checkout"))
+
+
+#     # else:
+#     #     return render_template("checkout.html")
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
