ea6062667a6ac89460722c6e58097590466428c8
AmmarAlani <60627873+AmmarAlani@users.noreply.github.com>
2021-04-01 18:24:09 -0500

    test cases implemented

--- test_main.py
+++ test_main.py
@@ -14,13 +14,23 @@ class TestingMain(unittest.TestCase):
         self.assertEqual(quotes, quotes)
 
     def test_checkout(self):
-        self.assertEqual(checkout, checkout)
+        self.assertEqual(checkout, "./templates/checkout.html")
 
     def test_history(self):
         self.assertEqual(history, history)
 
     def test_faq(self):
         self.assertEqual(faq, faq)
+
+    def test_create_connection(self):
+        self.assertEqual(create_connection, create_connection)
+
+    def test_genON(self):
+        self.assertEqual(genON, genON)
+
+    def test_genUniqueID(self):
+        self.assertEqual(genUniqueID, genUniqueID)
+
 
 if __name__ == '__main__':
     unittest.main()
